62bd4fa0e1d538162eb49b848d987831e54a7676
invreate <invreate@invreate.invreate.com>
2020-03-26 19:28:02 -0500

    hseq

--- system/config_override.php
+++ system/config_override.php
@@ -5,4 +5,5 @@ $sugar_config['disabled_languages'] = '';
 $sugar_config['default_date_format'] = 'Y-m-d';
 $sugar_config['default_language'] = 'es_ES';
 $sugar_config['default_time_format'] = 'H:i';
+$sugar_config['dbconfig']['db_name'] = 'ns_hseq';
 /***CONFIGURATOR***/
