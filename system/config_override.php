d57ad293c71abae0b93a3255d7cae3012e164b8e
n3mod <martinezosoriod@gmail.com>
2020-05-29 15:00:59 -0500

    bug 0001: correción campo indicadores

--- system/config_override.php
+++ system/config_override.php
@@ -5,4 +5,9 @@ $sugar_config['disabled_languages'] = '';
 $sugar_config['default_date_format'] = 'Y-m-d';
 $sugar_config['default_language'] = 'es_ES';
 $sugar_config['default_time_format'] = 'H:i';
+$sugar_config['dbconfig']['db_name'] = 'ns_test';
+$sugar_config['email_allow_send_as_user'] = false;
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['dashlet_auto_refresh_min'] = '30';
+$sugar_config['stack_trace_errors'] = false;
 /***CONFIGURATOR***/
